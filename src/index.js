e2d4bdaac03f759ba7a58df3c97edd2070226744
Zach Frohardt <zfrohardt@gmail.com>
2021-04-08 15:11:03 -0700

    completed deliverables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", formHandler);
+}
+
+let formHandler = event => {
+    event.preventDefault();
+    let searchValue = event.target.querySelector("#searchByID").value;
+
+    fetch(`http://localhost:3000/movies/${searchValue}`)
+    .then(response => response.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
